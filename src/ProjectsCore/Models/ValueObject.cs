35ce3dce4020a9c227c509f1d76a712825aad358
ktomkow <krzysztof.tomkow@gmail.com>
2021-10-05 18:20:43 +0200

    First version of value object base class

---
+++ src/ProjectsCore/Models/ValueObject.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectsCore.Models
+{
+    public abstract class ValueObject<T> where T : class
+    {
+        public abstract bool Equals(T other);
+        public abstract override int GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is T other))
+            {
+                return false;
+            }
+
+            return this.Equals(other);
+        }
+    }
+}
