ec3f19e97427a0f5237c624d58cbedeaaeb71ff2
ktomkow <krzysztof.tomkow@gmail.com>
2021-10-23 16:53:27 +0200

    Moved mongo service provider

---
+++ src/ProjectsCore.Mongo.ServiceProvider/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using MongoPack.IdGeneration;
+using System;
+using ProjectsCore.Persistence;
+using MongoPack;
+using ProjectsCore.Mongo.Implementations;
+using ProjectsCore.Mongo.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectsCore.IdGeneration;
+using ProjectsCore.Mongo.Implementations.IdGeneration;
+
+namespace ProjectsCore.Mongo.ServiceProvider
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddMongo(this IServiceCollection services)
+        {
+            services.AddTransient<IDbFactory, DbFactory>();
+            services.AddTransient<ICollectionNameResolver, DefaultCollectionNameResolver>();
+
+            services.AddTransient(typeof(IRepository<,>), typeof(Repository<,>));
+
+            services.AddTransient<IEntityIdGenerator<int>, EntityIntIdGenerator>();
+            services.AddTransient<IEntityIdGenerator<Guid>, EntityGuidIdGenerator>();
+        }
+    }
+}
