80b351a22e5637c9c73a6f4ab8ed331574448742
ktomkow <krzysztof.tomkow@gmail.com>
2021-10-26 21:45:47 +0200

    Dynamic lambda factory

---
+++ src/ProjectsCore.DynamicLambda/DynamicLambdaFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Dynamic.Core;
+using System.Linq.Dynamic.Core.Parser;
+using System.Linq.Expressions;
+
+namespace ProjectsCore.DynamicLambda
+{
+    public static class DynamicLambdaFactory
+    {
+        public static Func<TInput, TOutput> CreateFunc<TInput, TOutput>(this string lambda)
+        {
+            ParameterExpression x = Expression.Parameter(typeof(TInput), "x");
+
+            var symbols = new[] { x };
+
+            Expression body = new ExpressionParser(symbols, lambda, symbols, new ParsingConfig()).Parse(typeof(TOutput));
+
+            LambdaExpression e = Expression.Lambda(body, new ParameterExpression[] { x });
+
+            Func<TInput, TOutput> func = e.Compile() as Func<TInput, TOutput>;
+
+            return func;
+        }
+    }
+}
---
+++ tests/unit/ProjectsCore.DynamicLambda.UnitTests/DynamicLambdaFactoryTests.cs
@@ -0,0 +1,46 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace ProjectsCore.DynamicLambda.UnitTests
+{
+    [Collection(nameof(DynamicLambdaFactoryTests))]
+    public class DynamicLambdaFactoryTests
+    {
+        [Fact]
+        public void Create_IntInt_ShouldWork()
+        {
+            // x => x * 2
+            string lambda = "x * 2";
+
+            Func<int, int> doubler = DynamicLambdaFactory.CreateFunc<int, int>(lambda);
+
+            int input = 3;
+            int expectedOutput = 6;
+
+            int output = doubler(input);
+
+            output.Should().Be(expectedOutput);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        public void Create_IntInt_ManyTimes_ShouldBeFast(int loopsCount)
+        {
+            for (int i = 0; i < loopsCount; i++)
+            {
+                // x => x * 2
+                string lambda = "x * 2";
+
+                Func<int, int> doubler = DynamicLambdaFactory.CreateFunc<int, int>(lambda);
+
+                int input = 3;
+                int output = doubler(input);
+            }
+        }
+    }
+}
