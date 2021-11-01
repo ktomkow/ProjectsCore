26b4cabe35286b31c47b6b0baed222bfbcc9f7a6
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-01 22:11:59 +0100

    Caller resolver - resolve class name of method caller

---
+++ src/ProjectsCore.Reflections/CallerResolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ProjectsCore.Reflections
+{
+    public static class CallerResolver
+    {
+        public static string ResolveName()
+        {
+            string name;
+            Type declaringType;
+            int skipFrames = 1;
+            do
+            {
+                MethodBase method = new StackFrame(skipFrames, false).GetMethod();
+                declaringType = method.DeclaringType;
+                if (declaringType == null)
+                {
+                    return method.Name;
+                }
+                skipFrames++;
+                name = declaringType.Name;
+            }
+            while (declaringType.Module.Name.Equals("mscorlib.dll", StringComparison.OrdinalIgnoreCase));
+
+            return name;
+        }
+
+        public static string ResolveFullName()
+        {
+            string fullName;
+            Type declaringType;
+            int skipFrames = 1;
+            do
+            {
+                MethodBase method = new StackFrame(skipFrames, false).GetMethod();
+                declaringType = method.DeclaringType;
+                if (declaringType == null)
+                {
+                    return method.Name;
+                }
+                skipFrames++;
+                fullName = declaringType.FullName;
+            }
+            while (declaringType.Module.Name.Equals("mscorlib.dll", StringComparison.OrdinalIgnoreCase));
+
+            return fullName;
+        }
+    }
+}
---
+++ tests/unit/ProjectsCore.Reflections.UnitTests/CallerResolverTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ProjectsCore.Reflections.UnitTests
+{
+    public class CallerResolverTests
+    {
+        [Fact]
+        public void ResolveName_WhenCalledInsideMethodOfSomeKindOfClass_ThenSomeKindOfClass()
+        {
+            SomeKindOfClass someObject = new SomeKindOfClass();
+
+            string result = someObject.ExecResolveName();
+
+            result.Should().Be(nameof(SomeKindOfClass));
+        }
+
+        [Fact]
+        public void ResolveFullName_WhenCalledInsideMethodOfSomeKindOfClass_ThenNamespaceAndClass()
+        {
+            SomeKindOfClass someObject = new SomeKindOfClass();
+
+            string result = someObject.ExecResolveFullName();
+
+            result.Should().Be($"ProjectsCore.Reflections.UnitTests.{nameof(SomeKindOfClass)}");
+        }
+
+        [Fact]
+        public void StaticResolvedName_WhenCalledInsideMethodOfSomeKindOfClass_ThenSomeKindOfClass()
+        {
+            SomeKindOfClass someObject = new SomeKindOfClass();
+
+            string result = someObject.ExecResolveName();
+
+            result.Should().Be(nameof(SomeKindOfClass));
+        }
+
+        [Fact]
+        public void StaticResolvedFullName_WhenCalledInsideMethodOfSomeKindOfClass_ThenNamespaceAndClass()
+        {
+            SomeKindOfClass someObject = new SomeKindOfClass();
+
+            string result = someObject.ExecResolveFullName();
+
+            result.Should().Be($"ProjectsCore.Reflections.UnitTests.{nameof(SomeKindOfClass)}");
+        }
+    }
+
+    internal class SomeKindOfClass
+    {
+        internal static readonly string ResolvedName = CallerResolver.ResolveName();
+        internal static readonly string ResolvedFullName = CallerResolver.ResolveFullName();
+
+        internal string ExecResolveName()
+        {
+            return CallerResolver.ResolveName();
+        }
+
+        internal string ExecResolveFullName()
+        {
+            return CallerResolver.ResolveFullName();
+        }
+    }
+}
